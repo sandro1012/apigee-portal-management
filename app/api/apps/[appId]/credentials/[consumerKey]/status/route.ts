03064e52d92246d399806ebcfe0ebe734cfeba9d
sandro1012 <sandro.roberto@gmail.com>
2025-08-23 20:30:20 -0300

    fix(apps): correct resolveApp import depth for status route

--- app/api/apps/[appId]/credentials/[consumerKey]/status/route.ts
+++ app/api/apps/[appId]/credentials/[consumerKey]/status/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { resolveDevAndApp } from "../../../../../../lib/util/resolveApp";
+import { resolveDevAndApp } from "../../../../../../../lib/util/resolveApp";
 
 export async function POST(req: Request, { params }: { params: { appId: string, consumerKey: string } }) {
   try {
