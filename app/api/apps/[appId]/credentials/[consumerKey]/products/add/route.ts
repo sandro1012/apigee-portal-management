9e9e7db3bdbfd53fb94c748ad93f9624712ac541
sandro1012 <sandro.roberto@gmail.com>
2025-08-31 11:04:28 -0300

    fix(api): corrigir caminhos de import de resolveApp

--- app/api/apps/[appId]/credentials/[consumerKey]/products/add/route.ts
+++ app/api/apps/[appId]/credentials/[consumerKey]/products/add/route.ts
@@ -1,5 +1,5 @@
 ﻿import { NextResponse } from "next/server";
-import { resolveApp as resolveDevAndApp, readBearer, buildKeyUrls } from "../../../../../../../lib/util/resolveApp";
+import { resolveApp as resolveDevAndApp, readBearer, buildKeyUrls } from "../../../../../../lib/util/resolveApp";
 
 export async function POST(req: Request, { params }: { params: { appId: string, consumerKey: string } }) {
   try {
